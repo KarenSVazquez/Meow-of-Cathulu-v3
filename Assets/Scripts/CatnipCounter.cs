eb2bb5567627bfaa3174bb2c101a6aa3f6a9f243
Lovaro <61359171+Lovaro@users.noreply.github.com>
2023-11-17 00:26:37 -0300

    npc_interactions

--- Assets/Scripts/CatnipCounter.cs
+++ Assets/Scripts/CatnipCounter.cs
@@ -1,4 +1,4 @@
-using System;
+// Script CatnipCounter
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,33 +6,34 @@ using UnityEngine.UI;
 
 public class CatnipCounter : MonoBehaviour
 {
-    public GameObject thePlayer;
     public int catnipCount = 0;
 
-    void Start()
-    {
-        thePlayer = GameObject.FindWithTag("Player");
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        Text catnipCounterCanvas = GameObject.FindWithTag("CatnipCounterCanvas").GetComponent<Text>();
-        catnipCounterCanvas.text = catnipCount.ToString();
+        UpdateCatnipCounterUI();
     }
 
+    public bool HasEnoughCatnips(int requiredCatnips)
+    {
+        return catnipCount >= requiredCatnips;
+    }
+
+    public void SubtractCatnips(int amount)
+    {
+        catnipCount -= amount;
+        UpdateCatnipCounterUI();
+    }
+
+    void UpdateCatnipCounterUI()
+    {
+        Text catnipCounterCanvas = GameObject.FindWithTag("CatnipCounterCanvas")?.GetComponent<Text>();
+        if (catnipCounterCanvas != null)
+        {
+            catnipCounterCanvas.text = catnipCount.ToString();
+        }
+    }
     public void AddOneCatnip()
     {
         catnipCount++;
-    }
-
-    internal void SubtractCatnips(int requiredCatnip)
-    {
-        throw new NotImplementedException();
-    }
-
-    internal bool HasEnoughCatnips(int requiredCatnip)
-    {
-        throw new NotImplementedException();
     }
 }
