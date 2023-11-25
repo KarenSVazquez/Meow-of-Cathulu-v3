2eace63d75048699ee07e2d81ebd5532df9e373e
Lovaro <61359171+Lovaro@users.noreply.github.com>
2023-11-24 21:48:22 -0300

    add_Scripts_npcSouls

--- Assets/Scripts/NPCCatnipsNeeded.cs
+++ Assets/Scripts/NPCCatnipsNeeded.cs
@@ -1,7 +1,7 @@
-// Script NPCCatnipsNeeded
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCCatnipsNeeded : MonoBehaviour
 {
@@ -13,24 +13,89 @@ public class NPCCatnipsNeeded : MonoBehaviour
     public float destroyDelay = 1.0f;
     public SO_CatCounting catCounting;
     public Sprite[] dialogueImages;
-    public bool canInteract = false;
 
+    // Datos específicos del NPC
+    public string npcName;
+    public int catnipsNeeded;
 
     // Variable para realizar el seguimiento de si ya se restó una vez
     private bool catnipSubtracted = false;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        catnipImage.sprite = sadFaceSprite;
-        canInteract = true;
+        if (!catnipSubtracted && other.tag == "Player")
+        {
+            CatnipCounter catnipCounterScript = GameObject.FindWithTag("CatnipCounter").GetComponent<CatnipCounter>();
+
+            if (catnipCounterScript != null && catnipCounterScript.HasEnoughCatnips(requiredCatnip) && _satisfied == false)
+            {
+                catnipCounterScript.SubtractCatnips(requiredCatnip);
+                requiredCatnip = 0;
+                _satisfied = true;
+
+                catnipImage.sprite = happyFaceSprite;
+                Invoke("DestroyNPC", destroyDelay);
+                Debug.Log("Antes ");
+
+                // Accede a los datos específicos del NPC directamente aquí
+                Debug.Log("Nombre del NPC: " + npcName);
+                Debug.Log("Catnips necesarios: " + catnipsNeeded);
+
+                DesactivarImagenesNPCSouls();
+                Debug.Log("Después ");
+
+                catnipSubtracted = true;
+            }
+            else
+            {
+                catnipImage.sprite = sadFaceSprite;
+            }
+        }
     }
 
-    void OnTriggerExit2D(Collider2D other)
+    void DesactivarImagenesNPCSouls()
     {
-        catnipImage.sprite = null;
-        canInteract = false;
+        // Obtén la referencia al componente NPCData
+        NPCData npcData = GetComponent<NPCData>();
 
+        if (npcData != null)
+        {
+            // Obtén la referencia al objeto del canvas asociado al NPC actual
+            GameObject canvasObject = npcData.canvasObject;
+
+            if (canvasObject != null)
+            {
+                // Desactivar el componente Image (si existe)
+                Image imageComponent = canvasObject.GetComponent<Image>();
+
+                if (imageComponent != null)
+                {
+                    imageComponent.enabled = false;
+                }
+
+                // Desactivar el componente MeshRenderer (si existe)
+                MeshRenderer meshRendererComponent = canvasObject.GetComponent<MeshRenderer>();
+
+                if (meshRendererComponent != null)
+                {
+                    meshRendererComponent.enabled = false;
+                }
+
+                // Desactivar el objeto completo
+                canvasObject.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("campo 'canvasObject' en NPCData es null.");
+            }
+        }
+        else
+        {
+            Debug.LogError("componente NPCData no está en NPC.");
+        }
     }
+
+
 
     void DestroyNPC()
     {
@@ -46,23 +111,5 @@ public class NPCCatnipsNeeded : MonoBehaviour
         catCounting.IsCathuluVisible = true;
         catCounting.NPCPosition = transform.position; // guarda la posición del NPC
         Destroy(gameObject);
-    }
-
-    private void Update()
-    {
-        CatnipCounter catnipCounterScript = GameObject.FindWithTag("CatnipCounter").GetComponent<CatnipCounter>();
-        if (canInteract && Input.GetButtonDown("Activate") && !catnipSubtracted)
-        {
-            if (catnipCounterScript != null && catnipCounterScript.HasEnoughCatnips(requiredCatnip) && _satisfied == false)
-            {
-                catnipSubtracted = true;
-                catnipCounterScript.SubtractCatnips(requiredCatnip);
-                requiredCatnip = 0;
-                _satisfied = true;
-                Invoke("DestroyNPC", destroyDelay);
-               
-            }
-        }
-
     }
 }
---
+++ Assets/Scripts/NPCData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class NPCData : MonoBehaviour
+{
+    public GameObject canvasObject;
+   
+}
